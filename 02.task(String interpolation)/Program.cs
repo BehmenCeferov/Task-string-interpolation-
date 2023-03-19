aea07752d69b2d185f315ae25cd22921021a2957
ASUS <ASUS@DESKTOP-S7U2CS1>
2023-03-19 16:21:40 +0400

    Add project files.

---
+++ 02.task(String interpolation)/Program.cs
@@ -0,0 +1,9 @@
+﻿using static System.Console;
+WriteLine("What language are you learn?");
+string programmingLanguage = ReadLine();
+WriteLine("What is your group number?");
+string groupNumber = ReadLine();
+WriteLine("Who is your best friend?");
+string bestFriend = ReadLine();
+string text = $"Programming language that I learn: {programmingLanguage}. My group's number is {groupNumber}. My Best friend is {bestFriend}";
+WriteLine(text);
