d7fc2b92ced84e4ab8ab8aef5580fea22db51b6f
Behmen <behmenceferov6@gmail.com>
2023-03-19 17:46:56 +0400

    2nd task

---
+++ 03.task(.MathOperations)/Program.cs
@@ -0,0 +1,15 @@
+﻿using static System.Console;
+using static System.Math;
+using static System.Convert;
+
+int numberOne = 10;
+int numberTwo = 5;
+WriteLine($"Write any number: ");
+string numberThree = ReadLine();
+int convertedNumberThree = ToInt32(numberThree);
+WriteLine($"{numberOne} - {numberThree} = {numberOne - convertedNumberThree}"); 
+WriteLine($"{numberOne} + {numberThree} = {numberOne + convertedNumberThree}"); 
+WriteLine($"{numberOne} * {numberThree} = {numberOne * convertedNumberThree}"); 
+WriteLine($"{numberTwo} - {numberThree} = {numberTwo - convertedNumberThree}"); 
+WriteLine($"{numberTwo} + {numberThree} = {numberTwo + convertedNumberThree}"); 
+WriteLine($"{numberTwo} * {numberThree} = {numberTwo * convertedNumberThree}");
