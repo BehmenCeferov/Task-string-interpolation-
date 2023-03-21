e5d61adc8e48bda6550e1b88715e597a0fc0b037
Behmen <behmenceferov6@gmail.com>
2023-03-21 18:58:07 +0400

    Added task

--- 03.task(.MathOperations)/Program.cs
+++ 03.task(.MathOperations)/Program.cs
@@ -1,15 +1,25 @@
-﻿using static System.Console;
-using static System.Math;
-using static System.Convert;
+﻿//using static System.Console;
+//using static System.Math;
+//using static System.Convert;
 
-int numberOne = 10;
-int numberTwo = 5;
-WriteLine($"Write any number: ");
-string numberThree = ReadLine();
-int convertedNumberThree = ToInt32(numberThree);
-WriteLine($"{numberOne} - {numberThree} = {numberOne - convertedNumberThree}"); 
-WriteLine($"{numberOne} + {numberThree} = {numberOne + convertedNumberThree}"); 
-WriteLine($"{numberOne} * {numberThree} = {numberOne * convertedNumberThree}"); 
-WriteLine($"{numberTwo} - {numberThree} = {numberTwo - convertedNumberThree}"); 
-WriteLine($"{numberTwo} + {numberThree} = {numberTwo + convertedNumberThree}"); 
-WriteLine($"{numberTwo} * {numberThree} = {numberTwo * convertedNumberThree}");
+//int numberOne = 10;
+//int numberTwo = 5;
+//WriteLine($"Write any number: ");
+//string numberThree = ReadLine();
+//int convertedNumberThree = ToInt32(numberThree);
+//WriteLine($"{numberOne} - {numberThree} = {numberOne - convertedNumberThree}"); 
+//WriteLine($"{numberOne} + {numberThree} = {numberOne + convertedNumberThree}"); 
+//WriteLine($"{numberOne} * {numberThree} = {numberOne * convertedNumberThree}"); 
+//WriteLine($"{numberTwo} - {numberThree} = {numberTwo - convertedNumberThree}"); 
+//WriteLine($"{numberTwo} + {numberThree} = {numberTwo + convertedNumberThree}"); 
+//WriteLine($"{numberTwo} * {numberThree} = {numberTwo * convertedNumberThree}");
+
+using System.Runtime.CompilerServices;
+using static System.Console;
+using static System.Math;
+//int numberOne = 11, numberTwo = 10;
+//WriteLine($"{numberOne} - {numberTwo} = {numberOne - numberTwo}");
+
+double x = 10, z = 2;
+double y = (Abs(((Pow(x, 2) * Pow(z, 4 / 3)) / (Pow(z, 3) * Pow(x, 2))) - Abs(Pow(x, 2))));
+WriteLine(y);
