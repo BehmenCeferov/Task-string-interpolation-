28deb6c87e34e5d0333c5e620c94e0a42f2e5273
Behmen <behmenceferov6@gmail.com>
2023-04-07 05:38:40 +0400

    array

---
+++ Array/Program.cs
@@ -0,0 +1,17 @@
+﻿using System.Text;
+
+Console.OutputEncoding = Encoding.UTF8;
+string[] students = new string[5]; 
+
+
+for (int i = 0; i < 5; i++)
+{
+    Console.Write($"{i + 1}-ci tələbənin adını daxil edin: ");
+    students[i] = Console.ReadLine(); 
+}
+
+Console.WriteLine("Daxil edilən tələbələr:");
+foreach (string student in students)
+{
+    Console.WriteLine(student);
+}
