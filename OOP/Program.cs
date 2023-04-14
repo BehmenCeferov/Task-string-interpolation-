2fd7b42d7c64e6f623723bf1ea0a332c24ef7008
Behmen <behmenceferov6@gmail.com>
2023-04-14 18:24:17 +0400

    OOP

---
+++ OOP/Program.cs
@@ -0,0 +1,25 @@
+﻿using static System.Console;
+
+Phone phone = new Phone();
+phone.name = "Iphone";
+phone.model = "11 Pro";
+phone.price = 1300;
+phone.ShowNameAndModel();
+int monthlyPayment = phone.CalcCredit(6, 10);
+WriteLine("Monthly payment: " + monthlyPayment);
+public class Phone
+{
+    public string name;
+    public string model;
+    public int price;
+    public void ShowNameAndModel()
+    {
+        WriteLine($"Name: {name}, Model: {model}");
+    }
+    public int CalcCredit(int month, int persentage)
+    {
+        int monthlyPayment = (price / month) * (1 + persentage / 100);
+        WriteLine(monthlyPayment);
+        return monthlyPayment;
+    }
+}
