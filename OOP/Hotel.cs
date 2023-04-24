ed87f49686efd393c899ecf0f7894b169b0cc78b
Behmen <behmenceferov6@gmail.com>
2023-04-24 21:11:01 +0400

    Hotel OOP

---
+++ OOP/Hotel.cs
@@ -0,0 +1,40 @@
+﻿using OOP;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+Hotel hotel = new Hotel();
+hotel.HotelName = "Hilton Hotel";
+hotel.Adress = "1B Azadlig Avenue, Baku 1000";
+hotel.GetPriceRoomAndCount(120, 5);
+
+namespace OOP
+{
+    public class Hotel
+    {
+        private Guid Id;
+        public string HotelName;
+        public string Adress;
+        public Hotel()
+        {
+            Id = Guid.NewGuid();
+        }
+        public void GetPriceRoomAndCount(int roomPrice, int roomCount)
+        {
+            if ((roomCount < 1 || roomCount > 200) || (roomPrice < 60 || roomPrice > 150))
+            {
+                Console.WriteLine("Invalid room count or price!");
+            }
+            else
+            {
+                roomPrice *= roomCount;
+                Console.WriteLine($"Hotel Name: {HotelName} " +
+                    $"Hotel adress: {Adress} " +
+                    $"Room Id: {Id} " +
+                    $"RoomCount: {roomCount} " +
+                    $"RoomPrice: {roomPrice}");
+            }
+        }
+    }
+}
--- OOP/Program.cs
+++ OOP/Program.cs
@@ -1,25 +1,25 @@
-﻿using static System.Console;
+﻿//using static System.Console;
 
-Phone phone = new Phone();
-phone.name = "Iphone";
-phone.model = "11 Pro";
-phone.price = 1300;
-phone.ShowNameAndModel();
-int monthlyPayment = phone.CalcCredit(6, 10);
-WriteLine("Monthly payment: " + monthlyPayment);
-public class Phone
-{
-    public string name;
-    public string model;
-    public int price;
-    public void ShowNameAndModel()
-    {
-        WriteLine($"Name: {name}, Model: {model}");
-    }
-    public int CalcCredit(int month, int persentage)
-    {
-        int monthlyPayment = (price / month) * (1 + persentage / 100);
-        WriteLine(monthlyPayment);
-        return monthlyPayment;
-    }
-}
+//Phone phone = new Phone();
+//phone.name = "Iphone";
+//phone.model = "11 Pro";
+//phone.price = 1300;
+//phone.ShowNameAndModel();
+//int monthlyPayment = phone.CalcCredit(6, 10);
+//WriteLine("Monthly payment: " + monthlyPayment);
+//public class Phone
+//{
+//    public string name;
+//    public string model;
+//    public int price;
+//    public void ShowNameAndModel()
+//    {
+//        WriteLine($"Name: {name}, Model: {model}");
+//    }
+//    public int CalcCredit(int month, int persentage)
+//    {
+//        int monthlyPayment = (price / month) * (1 + persentage / 100);
+//        WriteLine(monthlyPayment);
+//        return monthlyPayment;
+//    }
+//}
